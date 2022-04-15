a6dbe5f1afe854b98e930d433f9a5fd70148da45
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2022-04-15 02:00:23 -0300

    Fix: UGC (0x6D) structs (#3)

--- Inbound/0x006D.py
+++ Inbound/0x006D.py
@@ -34,37 +34,52 @@ def decode_ugc_banner_post(): # CUgcBannerPost
     with Node("CUgcBannerPost"):
         # valid: 0,1,2,3,4,5,6,7,8,9,10,201,202,209 others cause exception
         add_byte("type")
-        add_unicode_str("unknown")
+        with Node("CUgcUserProxy"):
+            add_int("unk")
+            add_long("Account ID")
+            add_long("Character ID")
+            add_unicode_str("unk")
+            add_unicode_str("Character Name")
+        with Node("CUgcUploadProxy"):
+            add_long("UGC Uid")
+            add_unicode_str("UGC GUID")
+            add_byte("unk")
+            add_byte("unk")
+        add_long("Banner id")
+        count = add_byte("Hours")
+        for i in range(count):
+            decode_ugc_reserve(i)
+        add_unicode_str("UGC url")
 
-def decode_ugc_unknown():
-    add_unicode_str("unknown")
-    add_byte("unknown")
+def decode_ugc_reserve(i):
+    with Node("CUgcBannerPostReserve " + str(i)):
+        add_long("uuid")
+        add_int("2")
+        add_long("Banner id")
+        add_int("Date")
+        add_int("Hour")
+        add_long("0")
 
-def decode_ugc_reserve(): # CUgcBannerReserve
-    add_long("unknown")
-    count = add_int("count")
-    for i in range(count):
-        add_int("unknown")
-        add_long("unknown")
-        add_int("unknown")
-        add_int("unknown")
-        add_long("unknown")
+def decode_ugc_hour_post():
+    add_long("Date + Hour")
+    add_unicode_str("Character name")
+    add_byte("Reserved")
 
 f = add_byte("Function")
 if f == 0:
     pass
-elif f == 2:
-    add_byte("ugcType")
+if f == 2: # create UGC
+    add_byte("UGC Type")
     # if type is invalid -> invaild ugcType
-    add_long("unknown")
-    add_unicode_str("unknown")
+    add_long("UGC Uid")
+    add_unicode_str("UGC Guid")
     # error PreUpload
     # error soap upload
-elif f == 4:
-    add_byte("ugcType")
-    add_long("unknown")
+elif f == 4: # set url
+    add_byte("UGC Type")
+    add_long("UGC Uid")
     # return codes 13=err, 91, 92
-    add_unicode_str("unknown")
+    add_unicode_str("UGC Url")
 elif f == 6:
     add_long("unknown")
     count = add_int("count")
@@ -75,12 +90,12 @@ elif f == 7:
     n = add_byte("unknown")
     if n == 1:
         decode_ugc_banner_post()
-elif f == 8:
-    add_long("unknown")
-    count = add_int("count")
-    for i in range(count):
-        add_long("unknown")
-        decode_ugc_unknown()
+elif f == 8: #update banner
+    add_long("Banner id")
+    hours = add_int("Hours")
+    for i in range(hours):
+        with Node("Hour " + str(i)):
+            decode_ugc_hour_post()
 elif f == 11:
     add_int("unknown")
     add_long("unknown")
@@ -98,7 +113,7 @@ elif f == 17:
     add_unicode_str("Endpoint 2")
     add_unicode_str("Locale")
     add_byte("unknown") # const?
-elif f == 18:
+elif f == 18: #load banners
     count = add_int("count")
     for i in range(count):
         b = add_bool("unknown")
@@ -106,18 +121,25 @@ elif f == 18:
             decode_ugc_banner_rolling_image()
     count = add_int("count")
     for i in range(count):
-        b = add_bool("unknown")
-        add_long("unknown")
-        if b:
-            decode_ugc_banner_post()
+        bannerId = add_long("Banner id")
+        with Node("Banner " + str(bannerId)):
+            active = add_byte("Active")
+            if active:
+                decode_ugc_banner_post()
     count = add_int("count")
     for i in range(count):
-        add_int("unknown")
-        add_long("unknown")
-        decode_ugc_unknown()
-elif f == 20:
-    decode_ugc_reserve()
-elif f == 21:
+        bannerId = add_long("Banner id")
+        with Node("Banner " + str(bannerId)):
+            hours = add_int("Hour count")
+            for j in range(hours):
+                with Node("Hour " + str(j)):
+                    decode_ugc_hour_post()
+elif f == 20:  # CUgcPreReserve
+    add_long("Banner id")
+    hours = add_int("Hours")
+    for i in range(hours):
+        decode_ugc_reserve(i)
+elif f == 21:  # CUgcGetCostTable
     count = add_int("count")
     for i in range(count):
         add_long("unknown")
---
+++ Outbound/0x0039.py
@@ -0,0 +1,56 @@
+'''UGC'''
+from script_api import *
+
+
+def decode_ugc_reserve(i):
+    with Node("CUgcBannerPostReserve " + str(i)):
+        add_long("uuid")
+        add_int("2")
+        add_long("Banner id")
+        add_int("Date")
+        add_int("Hour")
+        add_long("0")
+
+def decode_ugc_info():
+    type = add_byte("UGC Type")
+    add_byte("Unk")
+    add_byte("Unk")
+    add_int("Unk")
+    add_long("Account ID")
+    add_long("Character ID")
+    return type
+
+f = add_byte("Function")
+if f == 1: #create ugc
+    add_long("Unk")
+    type = decode_ugc_info()
+    add_long("Unk")
+    add_int("Unk")
+    add_short("Unk")
+    add_short("-256")
+    if type == 1 or type == 2: # item and furniture
+        add_long("Uid")
+        add_int("Item ID")
+        add_int("Amount")
+        add_unicode_str("Item Name")
+        add_byte("Unk")
+        add_long("Cost")
+        add_bool("Use voucher")
+    elif type == 3: #banners
+        add_long("Banner id")
+        hours = add_byte("Hours")
+        for i in range(hours):
+            decode_ugc_reserve(i)
+elif f == 3: #confirmation (?)
+    type = decode_ugc_info()
+    add_int("Unk")
+    add_long("UGC Uid")
+    add_unicode_str("UGC Guid")
+    add_short("-255")
+elif f == 18: #load banners
+    add_int("Map id")
+elif f == 19: #reserve banner
+    add_long("Banner id")
+    hours = add_int("Hours")
+    for i in range(hours):
+        decode_ugc_reserve(i)
