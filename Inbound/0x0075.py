f7aa614bb5f8e2cb26d6c9be065bdc492832ec45
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2022-06-19 15:32:28 -0300

    Update 0x0075.py (#7)

--- Inbound/0x0075.py
+++ Inbound/0x0075.py
@@ -2,13 +2,13 @@
 from script_api import *
 
 def decode_liftable(): # CLiftableObject
-    add_str("Unknown")
-    add_int("Unknown")
-    add_unicode_str("Unknown")
-    add_unicode_str("Unknown")
-    add_unicode_str("Unknown")
-    add_unicode_str("Unknown")
-    add_bool("Unknown")
+    add_str("CoordAsHexa")
+    add_int("State")  # Removed = 0, Active = 1, Disabled = 2
+    add_unicode_str("MaskQuestId")
+    add_unicode_str("MaskQuestState")
+    add_unicode_str("EffectQuestId")
+    add_unicode_str("EffectQuestState")
+    add_bool("Unknown") # Always 1?
 
 
 f = add_byte("Function")
@@ -16,21 +16,21 @@ if f == 0:
     count = add_int("count")
     for i in range(count):
         with Node("Liftable " + str(i), True):
-            add_str("Unknown")
-            add_byte("Unknown")
-            add_int("Unknown")
-            add_byte("Unknown")
-            add_unicode_str("Unknown")
-            add_unicode_str("Unknown")
-            add_unicode_str("Unknown")
-            add_unicode_str("Unknown")
-            add_bool("Unknown")
+            add_str("Entity ID")
+            add_byte("Enabled")
+            add_int("State") # Removed = 0, Active = 1, Disabled = 2
+            add_byte("PickedUp")
+            add_unicode_str("MaskQuestId")
+            add_unicode_str("MaskQuestState")
+            add_unicode_str("EffectQuestId")
+            add_unicode_str("EffectQuestState")
+            add_bool("Unknown") # Always 1?
 elif f == 2:
-    add_str("Unknown")
-    add_byte("Unknown")
-    add_int("Unknown")
-    add_byte("Unknown")
+    add_str("EntityId") # OR CoordAsHexa
+    add_byte("Enabled")
+    add_int("State") # Removed = 0, Active = 1, Disabled = 2
+    add_byte("PickedUp")
 elif f == 3:
     decode_liftable()
 elif f == 4:
-    add_str("Unknown")
+    add_str("CoordAsHexa")
