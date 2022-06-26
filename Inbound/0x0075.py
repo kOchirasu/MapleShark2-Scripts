e43aae3b392b090b5e9e1781b7f0d51d26aca75a
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2022-06-26 15:09:07 -0300

    Fix: liftable struct (#8)

--- Inbound/0x0075.py
+++ Inbound/0x0075.py
@@ -3,12 +3,12 @@ from script_api import *
 
 def decode_liftable(): # CLiftableObject
     add_str("CoordAsHexa")
-    add_int("State")  # Removed = 0, Active = 1, Disabled = 2
+    add_int("ItemCount")
     add_unicode_str("MaskQuestId")
     add_unicode_str("MaskQuestState")
     add_unicode_str("EffectQuestId")
     add_unicode_str("EffectQuestState")
-    add_bool("Unknown") # Always 1?
+    add_bool("UseEffect")
 
 
 f = add_byte("Function")
@@ -17,19 +17,19 @@ if f == 0:
     for i in range(count):
         with Node("Liftable " + str(i), True):
             add_str("Entity ID")
-            add_byte("Enabled")
-            add_int("State") # Removed = 0, Active = 1, Disabled = 2
-            add_byte("PickedUp")
+            add_byte("Unknown")
+            add_int("ItemCount") 
+            add_byte("State") # Active = 0, Removed = 1, Disabled = 2
             add_unicode_str("MaskQuestId")
             add_unicode_str("MaskQuestState")
             add_unicode_str("EffectQuestId")
             add_unicode_str("EffectQuestState")
-            add_bool("Unknown") # Always 1?
+            add_bool("UseEffect")
 elif f == 2:
     add_str("EntityId") # OR CoordAsHexa
-    add_byte("Enabled")
-    add_int("State") # Removed = 0, Active = 1, Disabled = 2
-    add_byte("PickedUp")
+    add_byte("Unknown")
+    add_int("ItemCount")
+    add_byte("State")  # Active = 0, Removed = 1, Disabled = 2
 elif f == 3:
     decode_liftable()
 elif f == 4:
